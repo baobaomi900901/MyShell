e31949ce273e7159b850e229785354b812ee2d37
MobyTang <baobaomi900901@icloud.com>
2026-03-17 17:53:20 +0800

    win: win_git.ps1 添加注释

--- Windows/win_git.ps1
+++ Windows/win_git.ps1
@@ -2,6 +2,7 @@
 # Git 简写命令
 
 function ga {
+    # 用途: 执行 "git add ."
     param(
         [Parameter(ValueFromRemainingArguments = $true)]
         [string[]]$Paths
@@ -23,12 +24,26 @@ Register-ArgumentCompleter -CommandName ga -ScriptBlock {
     } | Where-Object { $_ -like "*$wordToComplete*" }
 }
 
-function gs { git status }
-function gcmt { git commit -m $args }
-function gpr { git pull --rebase }
-function gpo { git push }
+function gs { 
+    # 用途: 执行 "git status"
+    git status 
+}
+
+function gcmt {
+    # 用途: 执行 "git commit -m"
+    git commit -m $args 
+}
+function gpr { 
+    # 用途: 执行 "git pull --rebase"
+    git pull --rebase 
+}
+function gpo { 
+    # 用途: 执行 "git push"
+    git push 
+}
 
 function greset {
+    # 用途: 执行 "git reset --hard" 或 "git reset --soft"
     param (
         [Parameter(Position = 0)]
         [ArgumentCompleter({
@@ -56,7 +71,19 @@ function greset {
     }
 }
 
-function gsuk { git stash -u -k }
-function gspop { git stash pop }
-function grecmt { git reset --soft HEAD~1 }
-function glocal { git log origin/develop..HEAD --oneline}
+function gsuk {
+    # 用途: 执行 "git stash -u -k", 忽略暂存区的更改
+    git stash -u -k 
+}
+function gspop { 
+    # 用途: 执行 "git stash pop", 恢复最近的暂存区更改
+    git stash pop 
+}
+function grecmt { 
+    # 用途: 执行 "git rebase -i HEAD~1", 编辑最近 n 次提交
+    git reset --soft HEAD~1 
+}
+function glocal { 
+    # 用途: 执行 "git branch -D local", 删除本地分支
+    git log origin/develop..HEAD --oneline
+}
--- Windows/win_pnpm.ps1
+++ Windows/win_pnpm.ps1
@@ -5,4 +5,3 @@ function pd { pnpm run dev }
 function pdoc { pnpm run docs:dev }
 function pa { pnpm add }
 function pi { pnpm install }
-function open { explorer $args }
